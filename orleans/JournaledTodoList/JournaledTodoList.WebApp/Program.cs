aa6f3ca2d4d305b1a74f4365f25a73be14f77ae8
Antony Briggs <anbriggs@contractor.ea.com>
2025-04-03 16:31:31 +0100

    change to use MongoDB grain storage

--- orleans/JournaledTodoList/JournaledTodoList.WebApp/Program.cs
+++ orleans/JournaledTodoList/JournaledTodoList.WebApp/Program.cs
@@ -1,13 +1,24 @@
 ﻿using JournaledTodoList.WebApp.Components;
 using JournaledTodoList.WebApp.Services;
+using Orleans.Providers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-builder.AddKeyedAzureTableClient("clustering");
-builder.AddKeyedAzureBlobClient("grain-state");
 builder.UseOrleans(siloBuilder =>
 {
+    var createShardKey = false;
+    siloBuilder.UseMongoDBClient("mongodb://admin:admin@localhost:27017");
+    siloBuilder.UseMongoDBClustering(options =>
+    {
+        options.DatabaseName = "OrleansTestApp";
+        options.CreateShardKeyForCosmos = createShardKey;
+    });
+    siloBuilder.AddMongoDBGrainStorageAsDefault(options =>
+    {
+        options.DatabaseName = "OrleansTestAppPubSubStore";
+        options.CreateShardKeyForCosmos = createShardKey;
+    });
     siloBuilder.AddLogStorageBasedLogConsistencyProviderAsDefault();
     siloBuilder.AddStateStorageBasedLogConsistencyProvider(name: Constants.StateStorageProviderName);
 });
